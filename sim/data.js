53a06402f81bf102e58b0a96ce835b5c8915c133
Daniel Tixier <dgtixier@gmail.com>
2015-06-07 16:37:23 -0700

    Two improvements
    - Tooltips work
    - Loading screen style

--- sim/data.js
+++ sim/data.js
@@ -2,19 +2,25 @@ define(["jquery", "chosen","pitcherData", "batterData"], function($, c, p, b) {
 
   // Bind players to dropdowns
   $(p).each(function(index, element) {
-      $('.pitcherselect').append($('<option>').data('player', element).text(element.nameFull + ", " + element.yearID + ", " + element.setID));
+      $('.pitcherselect')
+        .append($('<option>')
+        .data('player', element)
+        .text(element.nameFull + ", " + element.yearID + ", " + element.setID));
   });
-  $(b).each(function(index, element) {
-      $('.batterselect').append($('<option>').data('player', element).text(element.nameFull + ", " + element.yearID + ", " + element.setID));
+  $.each(b, function(index, element) {
+      $('.batterselect')
+        .append($('<option>')
+        .data('player', element)
+        .text(element.nameFull + ", " + element.yearID + ", " + element.setID));
   });
   
-  // Set up chosen dropdowns
+  //Set up chosen dropdowns
   $('.playerselect').chosen({
     disable_search_threshold: 10,
     no_results_text: "Oops, nothing found!",
     width: "85%"
   });
-  // Because chosen loads the data for a long time...
+  // Because data takes so long to load...
   $('.loading').removeClass('loading').empty();
 
 
